cd641a29172506a9fae8fb46fd324d5eb08e9040
Jon Cluck <cluckj@users.noreply.github.com>
2019-01-29 21:32:43 -0500

    Create config_radiofruit.go

---
+++ config_radiofruit.go
@@ -0,0 +1,12 @@
+// +build radiofruit
+
+package rfm69
+
+// Configuration for Raspberry Pi Zero W and Adafruit Radiofruit RFM69HCW.
+
+const (
+	spiDevice    = "/dev/spidev0.1"
+	spiSpeed     = 6000000 // Hz
+	interruptPin = 22      // GPIO for receive interrupts
+	resetPin     = 25      // GPIO for hardware reset
+)
